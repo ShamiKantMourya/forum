85131b28198373f146bf0d3813885996a79d306e
Shami_Mourya <88420924+ShamiKantMourya@users.noreply.github.com>
2023-06-24 23:57:31 +0530

    post

--- src/Post.js
+++ src/Post.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useReducer, useState, useEffect } from 'react';
 import { AiFillCaretDown, AiFillCaretUp } from "react-icons/ai";
-import {BiComment, BiShareAlt, BiBookmarks} from "react-icons/bi";
+import { BiComment, BiShareAlt, BiBookmarks } from "react-icons/bi";
 
 import "./Post.css";
 import { fakeFetch } from './database';
@@ -11,14 +11,18 @@ const Post = () => {
     const [newcolor, setNewColor] = useState("black");
 
     const getData = async () => {
-       try {
-        const response = await fakeFetch("https://database.com/api/data");
-        const data = response.data;
-        setPostData(data);
-       } catch (error) {
-        console.log(error)
-       }
+        try {
+            const response = await fakeFetch("https://database.com/api/data");
+            const data = response?.data[1]?.posts;
+            const userData = response?.data[0]?.User;
+            setPostData(data);
+            console.log(response);
+            console.log(userData);
+        } catch (error) {
+            console.log(error)
+        }
     };
+    console.log(postData);
     useEffect(() => {
         getData();
     }, [])
@@ -43,32 +47,34 @@ const Post = () => {
     return (
         <div>
             <div className='conatiner'>
-                <div className='vote-button'>
-                    <button onClick={() => dispatch({ type: "increment", payload: 1 })}><AiFillCaretUp /></button>
-                    <h2>{state.counter}</h2>
-                    <button onClick={() => dispatch({ type: "decrement", payload: 1 })}><AiFillCaretDown /></button>
-                </div>
                 {
-                    postData.map(({postId, post, tags,  postDescription})=>
-                     <div className='post-box' key={postId}>
-                    <div className='userdetails'>
+                    postData.map(({ postId, username, picUrl, name, post, tags, postDescription }) =>
+                        <div className='main-box' key={postId}>
+                            <div className='vote-button'>
+                                <button onClick={() => dispatch({ type: "increment", payload: 1 })}><AiFillCaretUp /></button>
+                                <h2>{state.counter}</h2>
+                                <button onClick={() => dispatch({ type: "decrement", payload: 1 })}><AiFillCaretDown /></button>
+                            </div>
+                            <div className='post-box'>
+                                <div className='userdetails'>
 
-                    </div>
-                    <div className='post-heading'>
-                        <h3>{post}</h3>
-                    </div>
-                    <div className='tags'>
-                        <p>{tags}</p>
-                    </div>
-                    <div className='post-details'>
-                        <p>{postDescription}</p>
-                    </div>
-                    <div className='icons'>
-                        <BiComment/>
-                        <BiShareAlt />
-                        <p onClick={changeColor} className='bookmark' style={{color: newcolor}}><BiBookmarks /></p>
-                    </div>
-                </div>
+                                </div>
+                                <div className='post-heading'>
+                                    <h3>{post}</h3>
+                                </div>
+                                <div className='tags'>
+                                    <p>{tags}</p>
+                                </div>
+                                <div className='post-details'>
+                                    <p>{postDescription}</p>
+                                </div>
+                                <div className='icons'>
+                                    <BiComment />
+                                    <BiShareAlt />
+                                    <p onClick={changeColor} className='bookmark' style={{ color: newcolor }}><BiBookmarks /></p>
+                                </div>
+                            </div>
+                        </div>
                     )
                 }
             </div>
